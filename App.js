87e659e725ccb2f04b741bf66041c008de71770c
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-07-21 21:50:26 +0530

    add vent handler

--- App.js
+++ App.js
@@ -1,16 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [textInput, setTextInput] = useState('');
+
+  const [todoList, setTodoList] = useState([]);
+
+  function textInputHandler(enteredText){
+    setTextInput(enteredText);
+  } 
+
+  function appTodoHandler(){
+   setTodoList(pev => [...pev, textInput]);
+
+  }
+
   return (
     <View style={styles.appContainer} >
-      <View style={styles.inputContainer} >
-        <TextInput style={styles.TextInput} placeholder='this is placeholder'/>
-        <Button title='Add todo'/>
 
-      </View>
-      <View>
-<Text> your todo</Text>
+      <View style={styles.inputContainer} >
+        <TextInput style={styles.TextInput} placeholder='this is placeholder' onChangeText={textInputHandler}/>
+        <Button color={'#3740FE'} title='Add todo' onPress={appTodoHandler}/>
+       </View>
+
+      <View style={styles.todoContainer}>
+        {todoList.map((todo) => <Text key={todo} >{todo}</Text>)}
       </View>
      
     </View>
@@ -19,12 +35,18 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
   inputContainer:{
- 
+     flex: 1,
     flexDirection:'row',
     justifyContent:'space-between',
+    alignContent:'center',
+    paddingBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc'
     
   },
   TextInput:{
@@ -32,8 +54,11 @@ const styles = StyleSheet.create({
     borderWidth:1,
     borderRadius:5,
     padding:5,
-    width: "80%",
+    width: "70%",
     marginRight: 10
+  },
+  todoContainer:{
+    flex: 9,
   }
  
 });
