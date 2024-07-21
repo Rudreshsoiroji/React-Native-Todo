3f51fafebc7b57c642bd336febcecd658ed4cac5
Rudresh Soiroji <rudreshsoiroji459@gmail.com>
2024-07-21 20:52:24 +0530

    initial commmit

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer} >
+      <View style={styles.inputContainer} >
+        <TextInput style={styles.TextInput} placeholder='this is placeholder'/>
+        <Button title='Add todo'/>
+
+      </View>
+      <View>
+<Text> your todo</Text>
+      </View>
+     
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    padding: 50
   },
+  inputContainer:{
+ 
+    flexDirection:'row',
+    justifyContent:'space-between',
+    
+  },
+  TextInput:{
+    borderColor:"#cccccc",
+    borderWidth:1,
+    borderRadius:5,
+    padding:5,
+    width: "80%",
+    marginRight: 10
+  }
+ 
 });
